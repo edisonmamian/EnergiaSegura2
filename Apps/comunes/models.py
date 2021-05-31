f8e895c069e24f31175a4f2675e369f3f4c2016f
edison mamian <edison.mamian@correounivalle.edu.co>
2021-05-30 21:31:57 -0500

    moelos de fases y analisis

--- Apps/comunes/models.py
+++ Apps/comunes/models.py
@@ -18,7 +18,50 @@ class TipoIdentificacion (models.Model):
     )
     estado = models.ForeignKey (
         Estado,
-        limit_choices_to={'nombre': 'Activo'},
+        limit_choices_to={'id': 1},
         on_delete=models.CASCADE,
         verbose_name='Estado'
     )
+
+class Departamentos (models.Model):
+    #Almacena el nombre del departamento
+    nombre = models.CharField(
+        max_length = 20,
+        null = False,
+        unique = True,
+        verbose_name = 'Departamento'
+    )
+    #Almacena el código de identificación del departamento
+    codigo_dane = models.CharField(
+        max_length = 2,
+        null = False,
+        unique = True,
+        verbose_name = 'Código DANE'
+    )
+
+    def __str__(self):
+        return self.nombre
+
+#modelo para las ciudades de Colombia
+class Ciudades (models.Model):
+    #Almacena el nombre de la ciudad
+    nombre = models.CharField(
+        max_length = 20,
+        null = False,
+        verbose_name = 'Ciudad'
+    )
+    #Almacena el departamento en el que está ubicado
+    departamento = models.ForeignKey(
+        Departamentos,
+        on_delete=models.CASCADE,
+        verbose_name = 'Departamento'
+    )
+    #Almacena el código de identificación de la ciudad
+    codigo_dane = models.CharField (
+        max_length = 5,
+        unique = True,
+        verbose_name = 'Código DANE'
+    )
+
+    def __str__(self):
+        return self.nombre
