a6b4ddf92228ca9d5b2998d2502fe68ac0e494da
edison mamian <edison.mamian@correounivalle.edu.co>
2021-06-01 20:54:01 -0500

    crear, listar y actualizar objetos de ensayo

--- Apps/comunes/models.py
+++ Apps/comunes/models.py
@@ -9,6 +9,9 @@ class Estado (models.Model):
         verbose_name = 'Estado'
     )
 
+    def __str__(self):
+        return self.nombre
+
 class TipoIdentificacion (models.Model):
     nombre = models.CharField (
         max_length = 20,
