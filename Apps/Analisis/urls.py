9b4b202e65c047d3c80e354f153f8029af878366
Edison Mamian <edison.mamian@correounivalle.edu.co>
2021-08-05 20:21:55 -0500

    retirar makemigrations

--- Apps/Analisis/urls.py
+++ Apps/Analisis/urls.py
@@ -3,6 +3,7 @@ from .views import *
 
 app_name = 'Analisis'
 urlpatterns = [
+    # este es un ejemplo para ver 
     path ('fases/crear/', CrearFase.as_view(), name = 'crear_fase'),
     path ('fases/editar/<int:pk>/', ActualizarFase.as_view(), name = 'editar_fase'),
     path ('crear/', CrearAnalisis.as_view(), name = 'crear'),
